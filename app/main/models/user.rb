b724d4d058474601a91e088062e214ef0c9539e0
Diana Frank <diana.frank22@gmail.com>
2016-02-19 15:23:28 -0500

    changes

--- app/main/models/user.rb
+++ app/main/models/user.rb
@@ -3,10 +3,10 @@
 class User < Volt::User
   # login_field is set to :email by default and can be changed to :username
   # in config/app.rb
-  field login_field
-  field :name
+  # field login_field
+  # field :name
 
-  validate login_field, unique: true, length: 8
-  validate :email, email: true
+  # validate login_field, unique: true, length: 8
+  # validate :email, email: true
 
 end
